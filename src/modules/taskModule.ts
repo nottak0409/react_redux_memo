810087a3738c7a01e55f8d4f759d13da0fe4a2b9
nottak0409 <nottak0409@gmail.com>
2021-07-08 06:55:38 +0900

    typescript型定義部分作成

--- src/modules/taskModule.ts
+++ src/modules/taskModule.ts
@@ -38,3 +38,52 @@ const initialState: IState = {
         isSuccess: false,
     },
 };
+
+const taskModule = createSlice({
+    initialState,
+    reducers: {
+        setAddTaskName: (state: IState, action: PayloadAction<string>) => {
+            state.addTask.name = action.payload;
+        },
+        setEditTaskById: (state: IState, action: PayloadAction<number>) => {
+            const task = state.tasks.find((t) => t.id === action.payload);
+            if (!task) {
+                return;
+            }
+            state.editTask = task;
+        },
+        setEditTaskName: (state: IState, action: PayloadAction<string>) => {
+            state.editTask.name = action.payload;
+        },
+        addTask: (state: IState) => {
+            if (!state.addTask.name) {
+                return;
+            }
+            state.tasks.push({
+                id: state.addTask.id,
+                name: state.addTask.name,
+                isExecuted: false,
+            });
+            state.addTask.id = state.addTask.id + 1;
+            state.addTask.name = "";
+        },
+        editTask: (state: IState) => {
+            const task = state.tasks.find((t) => t.id === state.editTask.id);
+            if (!task) {
+                return;
+            }
+            task.name = state.editTask.name;
+            state.editTask.id = -1;
+        },
+        endTask: (state: IState, action: PayloadAction<number>) => {
+            const task = state.tasks.find((t) => t.id === action.payload);
+            if (!task) {
+                return;
+            }
+            task.isExecuted = true;
+        },
+        setSaveState: (state: IState, action: PayloadAction<ILoadingState>) => {
+            state.saveState = action.payload;
+        },
+    },
+});
